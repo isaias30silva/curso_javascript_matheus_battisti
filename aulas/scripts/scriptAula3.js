4875aa268ca479c1831eb390253a39fb051e6a85
Isaias <99995191+isaias30silva@users.noreply.github.com>
2026-01-28 15:05:52 -0300

    aula 3 - particularidades javascript

---
+++ aulas/scripts/scriptAula3.js
@@ -0,0 +1,20 @@
+
+//ordem de execução do código
+console.log("Primeiro");
+
+console.log("Segundo");
+
+//tipagem fraca: o tipo da variável pode ser alterado ao longo do desenvolvimento do código
+var a = 1;
+a = "teste";
+
+console.log(a);
+
+//comentário de uma linha
+
+/*
+Comentário
+de 
+múltiplas
+linhas
+*/
